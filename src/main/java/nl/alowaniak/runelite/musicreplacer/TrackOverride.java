2fb6f77c6163da225b332bd374732bbe2e56ba91
alowaniak <2017509+alowaniak@users.noreply.github.com>
2026-02-25 01:41:12 +0100

    Normalize filename fixes #40

--- src/main/java/nl/alowaniak/runelite/musicreplacer/TrackOverride.java
+++ src/main/java/nl/alowaniak/runelite/musicreplacer/TrackOverride.java
@@ -1,6 +1,7 @@
 package nl.alowaniak.runelite.musicreplacer;
 
 import java.nio.file.Path;
+import java.text.Normalizer;
 import java.util.Map;
 import java.util.stream.Stream;
 
@@ -13,7 +14,7 @@ import lombok.Value;
  * Things that aren't too clean:
  * 	originalPath when downloaded
  *  fromLocal which could be deduced (or maybe have a hierarchy LocalOverride/RemoteOverride)
- *  coupling of name to path and not knowing it's extension (which is requiring nasty workarounds)
+ *  coupling of name to path and not knowing its extension (which is requiring nasty workarounds)
  */
 @Value
 class TrackOverride
@@ -26,6 +27,14 @@ class TrackOverride
 	boolean fromLocal;
 	Map<String, String> additionalInfo;
 
+	public String fileSystemNormalizedName()
+	{
+		return Normalizer.normalize(name, Normalizer.Form.NFKD)
+				.replaceAll("\\p{M}", "") // remove accents
+				.replaceAll("[\\\\/:*?\"<>|\\p{Cntrl}]", "_") // invalid chars (incl. tabs)
+				.replaceAll("^[. ]+|[. ]+$", ""); // trim dots/spaces
+	}
+
 	/**
 	 * Multiple possibilities because extension is unknown.
 	 *
@@ -33,6 +42,6 @@ class TrackOverride
 	 */
 	public Stream<Path> getPaths()
 	{
-		return MusicPlayer.PLAYER_PER_EXT.keySet().stream().map(e -> Tracks.MUSIC_OVERRIDES_DIR.toPath().resolve(name + e));
+		return MusicPlayer.PLAYER_PER_EXT.keySet().stream().map(e -> Tracks.MUSIC_OVERRIDES_DIR.toPath().resolve(fileSystemNormalizedName() + e));
 	}
 }
